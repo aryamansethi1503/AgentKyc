3b02bcadf213f7842beaac8a9956d42da3ca54e0
aryamansethi1503 <aryamansethi@google.com>
2025-09-16 16:47:51 +0530

    New Virtual env created and dependencies installed.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/aryamansethi/kyc_agent/venv
+set -gx VIRTUAL_ENV /home/aryamansethi/kyc_agent/AgentKyc/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
